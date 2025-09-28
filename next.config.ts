03b3112825de1ea5704e57ecae1d729287dbcabc
Khalfoun Mohamed El Mehdi <khalfounmohamedelmehdi@gmail.com>
2025-09-28 14:41:02 +0100

    eslint ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withVanillaExtract(nextConfig);
