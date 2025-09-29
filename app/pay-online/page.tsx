bb9bd03a6002c69acf02d5682c6fc259b2776930
Khalfoun Mohamed El Mehdi <khalfounmohamedelmehdi@gmail.com>
2025-09-29 16:14:03 +0100

    rename pay online and ways to pay pages components

--- app/pay-online/page.tsx
+++ app/pay-online/page.tsx
@@ -40,7 +40,7 @@ const faqItems: AccordionItem[] = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
 ];
-export default function WaysToPayPage() {
+export default function PayOnlinePage() {
   return (
     <>
       <Navbar />
